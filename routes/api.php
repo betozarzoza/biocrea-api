bcc55f68b8df31bf48e753e3c14af9ee2931b270
betozarzoza <betozarzoza@gmail.com>
2021-01-06 11:18:16 -0600

    getting started project

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('signup', 'App\Http\Controllers\AuthController@signup');
+
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'App\Http\Controllers\AuthController@logout');
+        Route::get('user', 'App\Http\Controllers\AuthController@user');
+    });
+});
+
+Route::group([
+  'prefix' => 'reset'
+], function() {
+    Route::post('token', 'App\Http\Controllers\AuthController@createResetPasswordToken');
+    Route::post('password', 'App\Http\Controllers\AuthController@resetPassword');
+});
